23f7db2927fc898acb90c78d91358251826554f0
LAB316 Student16 <LAB316_Student16@gau.local>
2019-09-23 18:31:51 +0400

    First commit

---
+++ Week1/src/com/gau/Main.java
@@ -0,0 +1,29 @@
+package com.gau;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+//        System.out.println("Hello World!!!");
+//        int m[]= new int[4];
+//        int m1[]= {5, 9, 6, 78};
+//        for(int i=0; i<m1.length; i++){
+//            System.out.println(m1[i]);
+//        }
+//        int k[];
+//        int x = 987;
+//        System.out.println(x+12);
+//        Random random = new Random();
+//        System.out.println(random.nextln(100));
+
+        String s;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        System.out.println(s);
+    }
+
+}
